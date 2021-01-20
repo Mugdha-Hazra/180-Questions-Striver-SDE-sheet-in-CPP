28ab7476fcb6e2f220a45ae1ae1623eb1522f076
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-01-20 08:11:37 +0530

    Create sort 012.cpp

---
+++ sort 012.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+void sort012(int[],int);
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+void sort012(int a[], int n)
+{ sort(a,a+n);
+
+}
