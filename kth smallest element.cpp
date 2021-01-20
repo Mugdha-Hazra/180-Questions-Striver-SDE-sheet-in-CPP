80b9f7311189153aa13a235c7b9975cab1f88809
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-01-20 07:46:08 +0530

    Create kth smallest element.cpp

---
+++ kth smallest element.cpp
@@ -0,0 +1,54 @@
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int kthSmallest(int *, int, int, int);
+ 
+int main()
+{
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        cout<<kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+//User function template for C++
+
+// arr : given array
+// l : starting index of the array i.e 0
+// r : ending index of the array i.e size-1
+// k : find kth smallest element and return using this function
+int kthSmallest(int arr[], int l, int r, int k)
+{ int p=0;
+    int m=r+1;
+    sort(arr,arr+m);
+    p=arr[k-1];
+    return p;
+}
+
+
+
+
+
+
+
+
+
+
