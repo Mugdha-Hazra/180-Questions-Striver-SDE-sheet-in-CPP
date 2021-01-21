9c232eaae3378e18608a4c36704da94d8a96d783
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-01-21 13:34:24 +0530

    Create template_return_index.cpp

---
+++ template_return_index.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+template <class T>
+int funu(T a[],int n)
+{
+               T c,m;
+               int i;
+               for(i=0;i<n;i++)
+               {
+                              if(a[i]>c)
+                              {c=a[i];
+                              m=i;}
+               }
+               return m;
+}
+int main()
+{
+int a[100],n,i;
+double b[100];
+cin>>n;
+for(i=0;i<n;i++)
+{
+cin>>a[i];
+}
+for(i=0;i<n;i++)
+{
+cin>>b[i];
+}
+cout<<funu<int>(a,n);
+cout<<"\n";
+cout<<funu<double>(b,n);
+return 0;
+}
