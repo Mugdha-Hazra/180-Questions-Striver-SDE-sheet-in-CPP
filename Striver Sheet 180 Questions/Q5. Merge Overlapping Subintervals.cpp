f2f8d6f5c15508c23007deba621a820d94776a48
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-08-07 00:50:27 +0530

    Update Q5. Merge Overlapping Subintervals.cpp

--- Striver Sheet 180 Questions/Q5. Merge Overlapping Subintervals.cpp
+++ Striver Sheet 180 Questions/Q5. Merge Overlapping Subintervals.cpp
@@ -26,6 +26,19 @@ intervals[i].length == 2
 My Approach
 -------------
 
+step1. we'll take a vector of vector that is a matrix of 2 columns(for starting point and ending point) and n rows name it intervals and sort it.
+step2. we'll make another vector of vectors for storing the resultant non overlapping intervals and name it ans.
+step3. now we'll check the size of interval matrix if it is zero the we'll return the ans matrix.
+step4. now we will make 2 variables 
+        1) f which we assing to the starting point of the interval 
+        2) s for ending point of the same interval
+step5.Now we'll iterate through all the elements and make two new var for storing current starting points (fn) and ending point(sn) of the interval and each time we'll check
+        1) if the current starting point is smaller than the s (that is if(fn<=s))
+           then we'll check the second point and set the s accordingly
+        2)else if the fn>s
+            then we'll put the f and s to the ans matrix and set s and f with the new valuees
+step6. at the end we'll put the final values to the ans matrix and return the ans matrix. 
+
 */
 class Solution {
 public:
