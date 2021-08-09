f74b50ddd3efa9f71314a1e889fffaecbd984876
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-08-09 20:56:19 +0530

    Create Q14.Pow(X,n).cpp

---
+++ Striver Sheet 180 Questions/Q14.Pow(X,n).cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        // base case
+        // +ve power
+        if(n == 0 || n == 1)
+            return n == 0 ? 1 : x;
+        // -ve power
+        if(n == -1)
+            return 1 / x;
+        
+        double power = myPow(x, n/2);
+        // if power n is even
+        if(n % 2 == 0)
+            return power * power;
+        // odd power
+        else {
+            return power * power * (n < 0 ? (1 / x) : x);
+        }
+    }
+};
