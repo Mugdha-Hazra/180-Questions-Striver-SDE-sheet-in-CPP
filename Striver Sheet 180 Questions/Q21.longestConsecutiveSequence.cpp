58b4077788cb507405abe05401f8f65e935b2fa6
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-08-13 22:02:01 +0530

    Create Q21.longestConsecutiveSequence.cpp

---
+++ Striver Sheet 180 Questions/Q21.longestConsecutiveSequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+         int n = nums.size();
+        if (!n) return 0;
+        sort(nums.begin(), nums.end());  // first sort the vector
+        int ans = 1, len = 1;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i + 1] == nums[i]) continue; //  ignore duplicates
+            if (nums[i + 1] == 1 + nums[i])
+                len++;  // add length of current sequence
+            else {
+                ans = max(ans, len);  // update max length
+                len = 1; // reset length of current sequencecurrent 
+            }
+        }
+        ans = max(ans, len);  // update ans with last sequence length
+        return ans;
+        
+    }
