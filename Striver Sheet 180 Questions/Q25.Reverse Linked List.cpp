d70162456f70d0cdd5c3aec7f345d440d3e9c25c
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-08-23 20:17:23 +0530

    Update Q25.Reverse Linked List.cpp

--- Striver Sheet 180 Questions/Q25.Reverse Linked List.cpp
+++ Striver Sheet 180 Questions/Q25.Reverse Linked List.cpp
@@ -11,3 +11,30 @@ ListNode* helper(ListNode* head, ListNode* prev) {
         return helper (head, NULL);
         
     }
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Reversing a Link list
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) 
+    {
+        ListNode* d=NULL;
+        while(head!=NULL)
+        {   ListNode* next=head->next;
+            head->next=d;
+            d=head;
+            head=next;
+        }
+        return d;
+        
+    }
+};
