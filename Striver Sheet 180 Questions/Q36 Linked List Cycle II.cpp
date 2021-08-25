ffce2bc61a156585cab16d07dd9ce5d1b88e60cd
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-08-25 22:21:54 +0530

    Create Q36 Linked List Cycle II.cpp

---
+++ Striver Sheet 180 Questions/Q36 Linked List Cycle II.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) 
+    {
+       if(head==NULL||head->next==NULL)
+           return NULL;
+        ListNode *slow=head,*fast=head,*entry=head;
+        while(fast->next&&fast->next->next)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+            {
+                while(slow!=entry)
+                {
+                    slow=slow->next;
+                    entry=entry->next;
+                }
+                return entry;
+            }
+        }
+        return NULL;
+    }
+};
