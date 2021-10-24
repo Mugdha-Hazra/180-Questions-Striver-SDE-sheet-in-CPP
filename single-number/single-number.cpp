49e4bb35575b9cc999bf0a15a46c4deab2802bac
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-10-24 22:23:31 +0530

    Time: 28 ms (39.23%), Space: 16.9 MB (85.58%) - LeetHub

---
+++ single-number/single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int c=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            c^=nums[i];
+        }
+        return c;
+    }
+};
