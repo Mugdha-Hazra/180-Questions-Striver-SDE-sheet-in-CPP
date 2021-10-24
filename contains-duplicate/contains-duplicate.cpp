e663e34ca4fa10b9cb34df4f896d1f076800b9ca
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-10-24 22:36:40 +0530

    Time: 20 ms (91.51%), Space: 15.3 MB (98.78%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+                return true;
+        }
+        return false;
+    }
+};
