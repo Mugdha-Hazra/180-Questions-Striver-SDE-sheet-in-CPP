9736035b2472db76c0343f219735e9b6a3430a62
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-10-31 10:48:07 +0530

    Time: 4 ms (41.45%), Space: 5.9 MB (47.76%) - LeetHub

---
+++ number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count=0;
+        while(n>0){
+            if(n & 1 == 1) count++;
+            n = n >> 1;
+        }
+        return count;
+    }
+};
