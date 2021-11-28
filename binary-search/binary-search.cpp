829d16f19eee8d2b140401fcfb7f653caef50d3e
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-11-28 13:38:24 +0530

    Time: 36 ms (70.14%), Space: 27.5 MB (92.64%) - LeetHub

--- binary-search/binary-search.cpp
+++ binary-search/binary-search.cpp
@@ -3,8 +3,13 @@ public:
     int search(vector<int>& a, int t) {
         int l=0,h=a.size()-1,m=0;
         while(l<=h)
-        {
-            int m=(l+h)/2;
+        { if(a[l]==t)
+            return l;
+         if(a[m]==t)
+             return m;
+         if(a[h]==t)
+             return h;
+             m=(l+h)/2;
             if(a[m]==t)
                 return m;
             else if(a[m]<t)
