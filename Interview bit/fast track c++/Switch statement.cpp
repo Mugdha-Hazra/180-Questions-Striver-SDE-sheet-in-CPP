12c8579beb4012ccea1e0108e00642e78f5d0b09
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-12-22 23:55:19 +0530

    Create Switch statement.cpp

---
+++ Interview bit/fast track c++/Switch statement.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    int weekday;
+    cin>>weekday;
+    switch(weekday)
+    {
+        case 1: cout<<"Monday\n";
+        break;
+        case 2: cout<<"Tuesday\n";
+        break;
+          case 3: cout<<"Wednesday\n";
+        break;
+          case 4: cout<<"Thrusday\n";
+        break;
+          case 5: cout<<"Friday\n";
+        break;
+          case 6: cout<<"Saturday\n";
+        break;
+          case 7: cout<<"Sunday\n";
+        
+    }
+    
+    return 0;
+}
