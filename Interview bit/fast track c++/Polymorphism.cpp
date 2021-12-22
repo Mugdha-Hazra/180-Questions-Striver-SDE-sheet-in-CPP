2f75927b207ee1046d92a28473f148437701813b
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-12-23 00:04:21 +0530

    Create Polymorphism.cpp

---
+++ Interview bit/fast track c++/Polymorphism.cpp
@@ -0,0 +1,27 @@
+/*
+#include<iostream>
+using namespace std;
+*/
+
+// YOUR CODE GOES HERE
+int compute(int a,int b)
+{
+    return abs(a-b);
+}
+int compute(int a)
+{
+    return a*a;
+}
+
+
+/*
+int main()  {
+    int x,y;
+    cin>>x>>y;
+    cout<<compute(x,y)<<endl;
+    int z;
+    cin>>z;
+    cout<<compute(z)<<endl;
+    return 0;
+}
+*/
