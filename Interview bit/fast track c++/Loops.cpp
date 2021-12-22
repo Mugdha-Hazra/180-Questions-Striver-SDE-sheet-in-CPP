2f28e3407c52a0456eb07876761b9350fd28d207
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-12-22 23:56:22 +0530

    Create Loops.cpp

---
+++ Interview bit/fast track c++/Loops.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()  {
+    int N;
+    cin>>N;
+    for(int i=0;i<N;i++)
+    {
+        if(i%2==1)
+        cout<<i<<"\n";
+    }
+    
+    return 0;
+}
