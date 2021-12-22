9e6d23f7961c0f4d682091a7cd43ce4c0647bb3d
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-12-22 23:58:12 +0530

    Create Passing parameters.cpp

---
+++ Interview bit/fast track c++/Passing parameters.cpp
@@ -0,0 +1,25 @@
+/*
+#include<iostream>
+using namespace std;
+*/
+
+// Your code goes here
+void compute(int &a,int &b,int &c)
+{
+    a=a*a*a;
+    b=b*b*b;
+    c=c*c*c;
+}
+
+
+/*
+int main()  {
+    int A, B, C;
+    cin>>A>>B>>C;
+    compute(A, B, C);
+    cout<<A<<endl;
+    cout<<B<<endl;
+    cout<<C<<endl;
+    return 0;
+}
+*/
