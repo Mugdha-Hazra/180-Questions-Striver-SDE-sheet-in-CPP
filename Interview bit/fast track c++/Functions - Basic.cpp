2875b08f69e792f39424b25981bcca32b61a9f2c
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-12-22 23:57:39 +0530

    Create Functions - Basic.cpp

---
+++ Interview bit/fast track c++/Functions - Basic.cpp
@@ -0,0 +1,22 @@
+/*
+#include<iostream>
+using namespace std;
+*/
+
+// Your Code goes here
+int compute(int a,int b)
+{
+    return a*a+b*b;
+}
+
+
+
+
+/*
+int main()  {
+    int A, B;
+    cin>>A>>B;
+    cout<<compute(A, B)<<endl;
+    return 0;
+}
+*/
