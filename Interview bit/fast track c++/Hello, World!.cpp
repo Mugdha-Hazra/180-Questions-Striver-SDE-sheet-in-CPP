7145baecce9013e96f34527cbd527993e11509bd
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-12-22 23:51:46 +0530

    Create Hello, World!.cpp

---
+++ Interview bit/fast track c++/Hello, World!.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    // YOUR CODE GOES HERE
+    // Please take input and print output to standard input/output (stdin/stdout)
+    // E.g. 'cin' for input & 'cout' for output
+    cout<<"Hello, InterviewBit!";
+    return 0;
+}
