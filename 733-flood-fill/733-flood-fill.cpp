eb5a637059f8090eb9ebb73ab572eeffb497cac6
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-01-27 00:06:43 +0530

    Time: 4 ms (97.90%), Space: 14 MB (81.31%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,22 +1,27 @@
 class Solution {
-public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor, set<pair<int, int>> &set, int &tmp) {
-        if(sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size() || image[sr][sc] != tmp || set.find({sr, sc}) != set.end())
-            return image;
-    
-        set.insert({sr, sc});
-        image[sr][sc] = newColor;
-        floodFill(image, sr - 1, sc, newColor, set, tmp);
-        floodFill(image, sr + 1, sc, newColor, set, tmp);
-        floodFill(image, sr, sc - 1, newColor, set, tmp);
-        floodFill(image, sr, sc + 1, newColor, set, tmp);
-        return image;
-    }
-    
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor)
+    void dfs(vector<vector<int>>& image, int sr, int sc,int newColor,int rows,int cols,int source)
     {
-        set<pair<int, int>> set;
-        int tmp = image[sr][sc];
-        return floodFill(image, sr, sc, newColor, set, tmp);    
+        if(sr<0 || sr>=rows || sc<0 || sc>=cols)
+            return;
+        else if(image[sr][sc]!=source)
+            return;
+        
+        image[sr][sc] = newColor;
+        
+        dfs(image,sr-1,sc,newColor,rows,cols,source);   //TOP
+        dfs(image,sr+1,sc,newColor,rows,cols,source);   //DOWN
+        dfs(image,sr,sc-1,newColor,rows,cols,source);   //LEFT
+        dfs(image,sr,sc+1,newColor,rows,cols,source);   //RIGHT
+    }
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        
+        if(newColor==image[sr][sc])
+            return image;
+        int rows = image.size();
+        int cols = image[0].size();
+        int source = image[sr][sc]; 
+        dfs(image,sr,sc,newColor,rows,cols,source);
+        return image;
     }
 };
