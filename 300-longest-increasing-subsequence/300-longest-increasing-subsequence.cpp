2b44cf4e111bf6dac45c3486207743c9da856d95
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-03-08 22:38:52 +0530

    Time: 8 ms (94.35%), Space: 10.4 MB (86.53%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& A) {
+        int len = 0;
+        for(auto cur : A) 
+            if(len == 0 || A[len-1] < cur) A[len++] = cur;             // extend
+            else *lower_bound(begin(A), begin(A) + len, cur) = cur;    // replace
+        return len;
+    }
+};
