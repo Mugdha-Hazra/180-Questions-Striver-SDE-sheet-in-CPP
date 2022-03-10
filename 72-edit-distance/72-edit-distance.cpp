786788fe264d384ca40d6a808158a6dccef1e7e4
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-03-10 22:45:05 +0530

    Time: 11 ms (79.38%), Space: 9.2 MB (19.29%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int n = word1.length(), m = word2.length();
-		
-		//Declaring two vectors to store the previous and the current state
-        vector<int>prev(m+1), curr(m+1);
-        
-		//In the case where one string is null, then it will require insertions of length(other string)
-        for(int i = 1; i <= m; i++)
-            prev[i] = i;
-        
-		
-        for(int i = 1;i <= n; i++){
-			//curr[0] = i sets the first element of ith row as i(same reason as above)
-            curr[0] = i;
-            for(int j = 1;j <= m; j++)
-			
-				/** If word1[i-1] == word2[j-1], i.e., if the current characters of both words match, 
-				then no need of any change, so curr[j] = prev[j-1].*/
-				/** Else, we can either delete, insert, or change the character of word1, which 
-				requires 1 step more than the minnimum of all the three operations possible. */
-				
-                curr[j] = (word1[i-1] == word2[j-1])? prev[j-1] : min(curr[j-1], min(prev[j], prev[j-1])) + 1;     
-				
-			//Swapping the prev and curr states so that current state becomes the prev state
-			//for next iteration.
-            prev.swap(curr);
-        }
-        return prev[m];
+      vector<vector<int>> dp(word1.length() + 1, vector<int>(word2.length() + 1, -1));  
+      return findMinOperationsRecursive(dp,word1, word2, 0, 0);
     }
+
+private:
+  int findMinOperationsRecursive(
+    vector<vector<int>>& dp,const string &s1, const string &s2, int i1, int i2) {
+    if(dp[i1][i2] == -1){
+
+    // if we have reached the end of s1, then we have to insert all the remaining characters of s2
+    if (i1 == s1.length()) {
+      dp[i1][i2] = s2.length() - i2;
+    }
+
+    // if we have reached the end of s2, then we have to delete all the remaining characters of s1
+    else if (i2 == s2.length()) {
+      dp[i1][i2] = s1.length() - i1;
+    }
+
+    // If the strings have a matching character, we can recursively match for the remaining lengths
+    else if (s1[i1] == s2[i2]) {
+      dp[i1][i2] = findMinOperationsRecursive(dp, s1, s2, i1 + 1, i2 + 1);
+      } else {
+        int c1 = findMinOperationsRecursive(dp, s1, s2, i1 + 1, i2);     // delete
+        int c2 = findMinOperationsRecursive(dp, s1, s2, i1, i2 + 1);     // insert
+        int c3 = findMinOperationsRecursive(dp, s1, s2, i1 + 1, i2 + 1); // replace
+        dp[i1][i2] = 1 + min(c1, min(c2, c3));
+      }
+    }
+  return dp[i1][i2];
+}
 };
