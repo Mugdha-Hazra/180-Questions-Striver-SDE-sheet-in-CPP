7ef1ac73bd803cfad7bf1dd80314cfa3a1d0435b
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-03-11 19:24:59 +0530

    Time: 15 ms (42.64%), Space: 10 MB (45.68%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int H = grid.size();
-        int W = grid[0].size();
-        vector<vector<int>> dp(H, vector<int>(W));
-        for(int row = 0; row < H; ++row) {
-            for(int col = 0; col < W; ++col) {
-                if(row == 0 && col == 0) {
-                    dp[row][col] = grid[row][col];
-                }
-                else {
-                    dp[row][col] = grid[row][col] + min((row==0?INT_MAX:dp[row-1][col]), (col==0?INT_MAX:dp[row][col-1]));
-                }
-            }
+        int rows = grid.size();
+        if(rows==0)
+            return 0;
+        int cols = grid[0].size();
+        vector<vector<int>> dp(rows,vector<int>(cols,0));//dp for top down
+        int i,j;
+        
+        dp[0][0] = grid[0][0];  //1st element is starting point the path sum is the same as the given path
+        //Fill 1st row
+        for(i=1;i<cols;++i)
+            dp[0][i] = dp[0][i-1] + grid[0][i];//since it will have only 1 option to fill
+        
+        //Fill 1st Col
+        for(i=1;i<rows;++i)
+            dp[i][0] = dp[i-1][0] + grid[i][0];//since it will have only 1 option to fill that is going downwards
+        
+        //Now fill the rest of the cell
+        for(i=1;i<rows;++i)
+        {
+            for(j=1;j<cols;++j)
+                dp[i][j] = grid[i][j] + min(dp[i-1][j],dp[i][j-1]);//it will have 2 options and v have to take the min of both .
         }
-        return dp[H-1][W-1];
+        return dp[rows-1][cols-1];
+        //finally return the last row and last col.
     }
 };
