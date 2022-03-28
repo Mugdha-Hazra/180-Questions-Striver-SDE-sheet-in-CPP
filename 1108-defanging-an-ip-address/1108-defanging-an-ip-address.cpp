248833b9ff16bdddca0c968ee34f88358f6727be
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-03-29 00:01:10 +0530

    Time: 4 ms (22.74%), Space: 6 MB (68.64%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -1,13 +1,12 @@
-class Solution {
+class Solution{
 public:
     string defangIPaddr(string address) {
-        string str="[.]";
-        for(int i=0;i<address.length();i++){
-            if(address[i]=='.'){
-                address.replace(i,1,str);
-                i+=2;
-            }
+        string res;
+        for(auto c: address) {
+            if(c=='.') {
+                res+="[.]";
+            } else res+=c;
         }
-        return address;
+        return res;
     }
 };
