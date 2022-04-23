1bc0a1e30ac046de9990f143cd0db010f634aae7
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-04-23 15:37:39 +0530

    Time: 16 ms (71.71%), Space: 13.2 MB (54.81%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        bool isZeroCol = false;
-        bool isZeroRow = false;
-        for (int i = 0; i < matrix.size(); i++) { //check the first column
-            if (matrix[i][0] == 0) {
-                isZeroCol = true;
+    void setZeroes(vector<vector<int>>& m) {
+        bool zc = false;
+        bool zr = false;
+        for (int i = 0; i < m.size(); i++) { //check the first column
+            if (m[i][0] == 0) {
+                zc = true;
                 break;
             } 
         }
-        for (int i = 0; i < matrix[0].size(); i++) { //check the first row
-            if (matrix[0][i] == 0) {
-                isZeroRow = true;
+        for (int i = 0; i < m[0].size(); i++) { //check the first row
+            if (m[0][i] == 0) {
+                zr = true;
                 break;
             } 
         }
-        for (int i = 1; i < matrix.size(); i++) { //check except the first row and column
-            for (int j = 1; j < matrix[0].size(); j++) 
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+        for (int i = 1; i < m.size(); i++) { //check except the first row and column
+            for (int j = 1; j < m[0].size(); j++) 
+                if (m[i][j] == 0) {
+                    m[i][0] = 0;
+                    m[0][j] = 0;
                 }
         }
-        for (int i = 1; i < matrix.size(); i++) { //process except the first row and column
-           for (int j = 1; j < matrix[0].size(); j++) 
-               if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                   matrix[i][j] = 0;
+        for (int i = 1; i < m.size(); i++) { //process except the first row and column
+           for (int j = 1; j < m[0].size(); j++) 
+               if (m[i][0] == 0 || m[0][j] == 0)
+                   m[i][j] = 0;
         }
-        if (isZeroCol) { //handle the first column
-            for (int i = 0; i < matrix.size(); i++) 
-                matrix[i][0] = 0;
+        if (zc) { //handle the first column
+            for (int i = 0; i < m.size(); i++) 
+                m[i][0] = 0;
         }
-        if (isZeroRow) { //handle the first row
-            for (int i = 0; i < matrix[0].size(); i++) 
-                matrix[0][i] = 0;
+        if (zr) { //handle the first row
+            for (int i = 0; i < m[0].size(); i++) 
+                m[0][i] = 0;
         }}
 };
