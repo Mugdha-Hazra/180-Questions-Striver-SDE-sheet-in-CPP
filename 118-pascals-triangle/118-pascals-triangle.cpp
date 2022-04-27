86723de60d195996577e463b2d77623d1368e7b8
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-04-27 17:10:24 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (33.55%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) 
+    vector<vector<int>> generate(int n) 
     {
-        vector<vector<int>> triangle(numRows);
-        triangle[0].push_back(1);
-        for(int i=1; i< numRows; i++){
-            triangle[i].push_back(1);
-            for( int j = 1; j < i; j++){
-                triangle[i].push_back(triangle[i-1][j-1]+triangle[i-1][j]);
+        vector<vector<int>> tri(n);
+        
+        tri[0].push_back(1); //manually inserting the 1st row
+        
+        for(int i=1; i< n; i++){
+            
+            tri[i].push_back(1); //1st col of every row
+            
+            for( int j = 1; j < i; j++){ //filling the inside col of every row
+                tri[i].push_back(tri[i-1][j-1]+tri[i-1][j]);
             }
-            triangle[i].push_back(1);                                      
+            tri[i].push_back(1);    //last col of every row                                  
         }
-        return triangle;
+        return tri; 
     }
 };
