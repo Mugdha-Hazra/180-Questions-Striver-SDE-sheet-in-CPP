fdbd006a5c121bee775945be3015f3472a6a7df5
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-04-29 18:55:21 +0530

    Time: 8 ms (34.17%), Space: 9.4 MB (95.74%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,15 +1,39 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int> > &mat, int t) {
-        int r=mat.size(), c=mat[0].size();
-        int l=0, h = c*r-1;
-        while (l< h){
-            int mid =(l+h-1)/2;
-            if (mat[mid/c][mid%c]<t)
-                l = mid + 1;
-            else 
-                h = mid;
+    
+    //bruteforce approach
+    
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+       /* for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                if(matrix[i][j]==target)
+                {
+                    return true;
+                }
+            }
         }
-        return mat[h/c][h%c] == t;
+        return false; */
+    
+    // using binary search
+    //find row =number of box / total number of box in  a row col= %total number of box in a row
+    int l=0;
+        int h=(matrix.size()*matrix[0].size())-1;
+        if(matrix.size()==0)
+            return 0;
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            if(matrix[mid/matrix[0].size()][mid%matrix[0].size()]==target)
+                return true;
+            
+            else if(matrix[mid/matrix[0].size()][mid%matrix[0].size()]>target){
+                h=mid-1;
+            }
+            
+            else l=mid+1;
+        }
+        return false;
     }
 };
