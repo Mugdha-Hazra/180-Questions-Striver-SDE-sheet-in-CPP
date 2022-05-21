e1d7ea8f7dd396d58880b57447a950e178858d21
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-05-21 20:10:02 +0530

    Time: 18 ms (12.62%), Space: 13.7 MB (95.47%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -3,15 +3,13 @@ public:
     int maxProduct(vector<int>& a) {
         int cp=1,ans=a[0];
         for(int i=0;i<a.size();i++)
-        {
-            cp*=a[i];
+        {   cp=cp*a[i];
             ans=max(cp,ans);
             if(cp==0) cp=1;
         }
         cp=1;
         for(int i=a.size()-1;i>=0;i--)
-        {
-            cp*=a[i];
+        {   cp=cp*a[i];
             ans=max(cp,ans);
             if(cp==0)cp=1;
         }
