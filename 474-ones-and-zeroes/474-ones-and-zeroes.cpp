4fc4bf3ddd52deae6c20b2eb84464ce91a0bf3e3
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-05-22 09:38:56 +0530

    Time: 149 ms (92.67%), Space: 8.8 MB (96.23%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    int findMaxForm(vector<string>& strs, int m, int n) {
-        int dp[m+1][n+1];
+    int findMaxForm(vector<string>& strs, int m, int n) 
+    {   int dp[m+1][n+1];
         memset(dp,0,sizeof(dp));
-        
-        for(int k=0; k<strs.size(); k++){
-            int w0=count(strs[k].begin(), strs[k].end(), '0');
+        for(int k=0; k<strs.size(); k++)
+        {   int w0=count(strs[k].begin(), strs[k].end(), '0');
             int w1=strs[k].size()-w0;
-            
-            for(int i=m; i>=w0; i--){
-                for(int j=n; j>=w1; j--){
-                    dp[i][j]=max(dp[i][j], dp[i-w0][j-w1]+1);
+            for(int i=m; i>=w0; i--)
+                {   for(int j=n; j>=w1; j--)
+                        dp[i][j]=max(dp[i][j], dp[i-w0][j-w1]+1);
                 }
-            }
         }
-        
         return dp[m][n];
     }
 };
