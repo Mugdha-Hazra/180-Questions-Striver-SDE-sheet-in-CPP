5f91f66eb8c28239d780099eda5c50d684dcde60
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-05-22 09:25:33 +0530

    Time: 245 ms (83.32%), Space: 9.7 MB (83.48%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<vector<int> > dp(n + 1, vector<int>(m + 1, 0));
-        int ones, zeros;
-        for (auto str : strs) {
-            ones = count(str.begin(), str.end(), '1');
-            zeros = count(str.begin(), str.end(), '0');
-            for (int i = n; i >= ones; i--) {
-                for (int j = m; j >= zeros; j--) {
-                    
-                    // In each cell, we check the max between using the current string or not.
-                    // dp[i][j] is the previous value in the cell - not using the current str.
-                    // dp[i - ones][j - zeros] is the value if we want to use the current string.
-                    // we add one because we are using another string.
-                    
-                    dp[i][j] = max(dp[i][j], dp[i - ones][j - zeros] + 1);
-                }
-            }
-        }
-        return dp[n][m];
+   int findMaxForm(vector<string>& strs, int m, int n) {
+  vector<vector<int>> memo(m+1, vector<int>(n+1, 0));
+  int numZeroes, numOnes;
+
+  for (auto &s : strs) {
+    numZeroes = numOnes = 0;
+    // count number of zeroes and ones in current string
+    for (auto c : s) {
+      if (c == '0')
+	numZeroes++;
+      else if (c == '1')
+	numOnes++;
     }
+
+    // memo[i][j] = the max number of strings that can be formed with i 0's and j 1's
+    // from the first few strings up to the current string s
+    // Catch: have to go from bottom right to top left
+    // Why? If a cell in the memo is updated(because s is selected),
+    // we should be adding 1 to memo[i][j] from the previous iteration (when we were not considering s)
+    // If we go from top left to bottom right, we would be using results from this iteration => overcounting
+    for (int i = m; i >= numZeroes; i--) {
+	for (int j = n; j >= numOnes; j--) {
+          memo[i][j] = max(memo[i][j], memo[i - numZeroes][j - numOnes] + 1);
+	}
+    }
+  }
+  return memo[m][n];
+}
+    
 };
