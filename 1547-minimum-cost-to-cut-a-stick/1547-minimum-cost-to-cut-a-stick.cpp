cfb52271f7838fd0af959aadf67e82711f9bccc2
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-05-22 15:19:40 +0530

    Time: 21 ms (99.45%), Space: 12 MB (12.24%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,35 +1,50 @@
 class Solution {
 public:
- 
-    vector <int> arr;
-    int dp[109][109];
- 
-    int solve(int idx, int jdx, int l, int r) {
-        if(idx < 0 || jdx >= arr.size() || idx > jdx)
-            return 0;
- 
-        if(idx == jdx)
-            return r - l;
- 
-        if(dp[idx][jdx] != -1)
-            return dp[idx][jdx];
- 
-        int ans = INT_MAX;
-        for(int i = idx; i <= jdx; i++) {
-            ans = min(ans, (r - l) + solve(idx, i - 1, l, arr[i]) + solve(i + 1, jdx, arr[i], r));
-        }
-        return dp[idx][jdx] = ans;
- 
-    }
- 
- 
- 
     int minCost(int n, vector<int>& cuts) {
- 
-        memset(dp, -1, sizeof dp);
+        // T[i, j] cost from point i to point j
+        // T[i, j] = min(T[i, k] + T[k, j]) + m[j] - m[i]
         sort(cuts.begin(), cuts.end());
-        this->arr = cuts;
- 
-        return solve(0, cuts.size() - 1, 0, n);
+        vector<int> arr(cuts.size() + 2);
+        arr[0] = 0;
+        arr[arr.size() - 1] = n;
+        for (int i=1; i < arr.size() - 1; ++i) {
+            arr[i] = cuts[i-1];
+        }
+        
+        
+        int m = arr.size();
+        vector<vector<int> > T(m, vector<int>(m, 0));
+        vector<vector<int> > C(m, vector<int>(m));
+        
+        // first diagnal
+        for (int i=0; i < m; ++i) C[i][i] = i;
+        
+        // second diagnal
+        for (int i=0; i < m-1; ++i) C[i][i+1] = i + 1;
+        
+        //cout << C[0][1] << ',' << C[1][2] << endl;
+        // third diagnal and above
+        // cout << arr.size() << endl;
+        for (int d=2; d < m; ++d) {
+            for (int i=0; i + d < m; ++i) {
+                int j = i + d;
+                
+                // C[i][j-1] <= C[i][j] <= c[i+1][j]
+                T[i][j] = INT_MAX;
+                int w = arr[j] - arr[i];
+                // cout << w << endl;
+                for (int k = max(i+1, C[i][j-1]); k <= min(j-1, C[i+1][j]); ++k) {
+                    int cost = T[i][k] + T[k][j] + w;
+                    if (T[i][j] >= cost) {
+                        C[i][j] = k;
+                        T[i][j] = cost;
+                    }
+                }
+                
+                //cout << i << ',' << j << ',' << T[i][j] << ',' << C[i][j] << endl;
+            }
+        }
+        
+        return T[0][m-1];
     }
 };
