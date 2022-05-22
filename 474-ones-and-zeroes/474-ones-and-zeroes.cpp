2c341338d449a461bd42f3d6cdb3d16dcc6793d2
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-05-22 09:33:54 +0530

    Time: 110 ms (94.86%), Space: 8.6 MB (98.80%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-   int findMaxForm(vector<string>& strs, int m, int n) {
-  vector<vector<int>> memo(m+1, vector<int>(n+1, 0));
-  int numZeroes, numOnes;
-
-  for (auto &s : strs) {
-    numZeroes = numOnes = 0;
-    // count number of zeroes and ones in current string
-    for (auto c : s) {
-      if (c == '0')
-	numZeroes++;
-      else if (c == '1')
-	numOnes++;
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        int dp[m+1][n+1];
+        memset(dp,0,sizeof(dp));
+        
+        for(int k=0; k<strs.size(); k++){
+            int w0=count(strs[k].begin(), strs[k].end(), '0');
+            int w1=strs[k].size()-w0;
+            
+            for(int i=m; i-w0>=0; i--){
+                for(int j=n; j-w1>=0; j--){
+                    dp[i][j]=max(dp[i][j], dp[i-w0][j-w1]+1);
+                }
+            }
+        }
+        
+        return dp[m][n];
     }
-
-    // memo[i][j] = the max number of strings that can be formed with i 0's and j 1's
-    // from the first few strings up to the current string s
-    // Catch: have to go from bottom right to top left
-    // Why? If a cell in the memo is updated(because s is selected),
-    // we should be adding 1 to memo[i][j] from the previous iteration (when we were not considering s)
-    // If we go from top left to bottom right, we would be using results from this iteration => overcounting
-    for (int i = m; i >= numZeroes; i--) {
-	for (int j = n; j >= numOnes; j--) {
-          memo[i][j] = max(memo[i][j], memo[i - numZeroes][j - numOnes] + 1);
-	}
-    }
-  }
-  return memo[m][n];
-}
-    
 };
