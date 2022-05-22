89db3cfd715d847ef5c57aa3a1c3e51b36763459
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-05-22 08:36:55 +0530

    Time: 8 ms (97.36%), Space: 10.6 MB (77.75%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,21 +4,22 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int m=text1.size(),n=text2.size();
         int dp[m+1][n+1];
-        for(int i=0;i<m+1;i++){
+        for(int i=0;i<m+1;i++){//making 1st row zero
             dp[i][0]=0;
         }
-        for(int i=0;i<n+1;i++){
+        for(int i=0;i<n+1;i++){//making 1st col 0
             dp[0][i]=0;
         }
+        //filling rest of the dp matrix
         for(int i=1;i<m+1;i++){
             for(int j=1;j<n+1;j++){
-               if(text1[i-1]==text2[j-1]){
-                  dp[i][j]= 1+dp[i-1][j-1];
-               }else{
-                   dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+               if(text1[i-1]==text2[j-1]){//if matching character
+                  dp[i][j]= 1+dp[i-1][j-1];//add 1 to the diagonal element
+               }else{//if not matching
+                   dp[i][j]=max(dp[i-1][j],dp[i][j-1]);//set max taking from left or top
                } 
             }
         }
-        return dp[m][n];
+        return dp[m][n];//return the last element of matrix.
     }
 };
