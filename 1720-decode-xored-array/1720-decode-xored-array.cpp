b18df6879de4fe4c7eb48271fcf19ef3d362dda8
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-05-27 20:55:00 +0530

    Time: 48 ms (27.67%), Space: 25 MB (73.46%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     vector<int> decode(vector<int>& a, int f) {
+       // a.push_front(f);
         a.insert(a.begin(),f);
         for(int i=0;i<a.size()-1;i++)
-        {
-            a[i+1]=a[i+1]^a[i];
+        {   f=a[i];
+            a[i+1]=a[i+1]^f;
         }
         return a;
     }
