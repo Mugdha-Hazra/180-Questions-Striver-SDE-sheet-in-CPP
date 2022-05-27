87f0184d8e58b5d5de16b40567a2386d1830eafe
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-05-27 20:45:48 +0530

    Time: 38 ms (60.02%), Space: 24.9 MB (89.66%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -4,7 +4,7 @@ public:
         a.insert(a.begin(),f);
         for(int i=0;i<a.size()-1;i++)
         {
-            a[i+1]^=a[i];
+            a[i+1]=a[i+1]^a[i];
         }
         return a;
     }
