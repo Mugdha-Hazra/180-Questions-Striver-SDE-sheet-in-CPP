429b075ad36f0975742e6c1f9a7396a45d89e55a
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-05-27 20:21:08 +0530

    Time: 3 ms (93.51%), Space: 10 MB (77.70%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -4,10 +4,10 @@ public:
         vector<int>a;
         for(int i=1;i<n.size();i=i+2)
         {
-            int c=n[i-1];
+            int c=n[i-1],k=n[i];
             while(c>0)
             {
-                a.push_back(n[i]);
+                a.push_back(k);
                 c--;
             }
         }
