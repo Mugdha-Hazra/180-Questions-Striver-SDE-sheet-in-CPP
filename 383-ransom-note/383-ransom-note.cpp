e4e1fed91de837a1294cf4bb0b48b72b3a912e6c
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-05-30 14:19:47 +0530

    Time: 24 ms (47.18%), Space: 8.8 MB (38.97%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -11,31 +11,14 @@ public:
         int a[26]={0};
         for(int i=0;i<m.size();i++)
         {
-            a[m[i]-'a']++;
+            a[m[i]-'a']=a[m[i]-'a']+1;
         }
         for(int i=0;i<r.size();i++)
         {
-            a[r[i]-'a']--;
+            a[r[i]-'a']=a[r[i]-'a']-1;
             if(a[r[i]-'a']<0)
                 return false;
         }
         return true;
     }
 };
-
-// sort(r.begin(),r.end());
-        // // sort(m.begin(),m.end());
-        // // if(r==m)
-        // //     return true;
-        // int f=0;
-        // for(int i=0;i<r.size();i++)
-        // {
-        //     if(r[i]==m[i])
-        //         f=1;
-        //     else
-        //     {f=0;
-        //     break;
-        //     }
-        // }
-        // if(f==0)
-        // return false;
