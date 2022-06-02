39d096e2f18df8243405c4b559067623f0ba0b5b
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-06-02 16:13:24 +0530

    Time: 7 ms (51.72%), Space: 9.8 MB (23.25%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -4,13 +4,10 @@ public:
         int l=0,h=a.size()-1,m=0;
         while(l<=h)
         {
-            int m=(l+h)/2;
-            if(a[m]==t)
-                return m;
-            else if(a[m]<t)
-                l=m+1;
-            else
-                h=m-1;
+             m=(l+h)/2;
+            if(a[m]==t) return m;
+            else if(a[m]<t)l=m+1;
+            else h=m-1;
         }
         return l;
         
