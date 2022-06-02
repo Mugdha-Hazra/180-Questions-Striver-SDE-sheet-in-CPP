2523c77e8875f05b77074d6421a7e855284e2492
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-06-02 10:35:39 +0530

    Time: 54 ms (40.98%), Space: 27.4 MB (99.80%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int search(vector<int>& a, int t) {
-        int n=a.size()-1;
-        int l=0,h=n,m=0;
-        if(t<a[0]||t>a[n])
-            return -1;
-        if(t==a[l])return l;
-        if(t==a[h])return h;
+        int l=0,h=a.size()-1,m=0;
         while(l<=h)
-        {
-            m=(l+h)/2;
-            if(t==a[m])
+        { if(a[l]==t)
+            return l;
+         if(a[m]==t)
+             return m;
+         if(a[h]==t)
+             return h;
+             m=(l+h)/2;
+            if(a[m]==t)
                 return m;
-            else if(t<a[m])
-                h=m-1;
-            else if(t>a[m])
+            else if(a[m]<t)
                 l=m+1;
+            else
+                h=m-1;
         }
-    return -1;
+        return -1;
     }
 };
