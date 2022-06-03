219b4573cacfef702a89107570dd98be1e47f86c
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-06-03 17:44:30 +0530

    Time: 31 ms (58.17%), Space: 19.2 MB (83.77%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& a)
+    {
+        int i=0,j=0;
+       while(j<a.size()&&i<a.size())
+        {
+            if(a[j]==0)
+                j++;
+            else 
+            {
+             swap(a[i++],a[j++]); 
+            }
+        }
+        
+    }
+};
