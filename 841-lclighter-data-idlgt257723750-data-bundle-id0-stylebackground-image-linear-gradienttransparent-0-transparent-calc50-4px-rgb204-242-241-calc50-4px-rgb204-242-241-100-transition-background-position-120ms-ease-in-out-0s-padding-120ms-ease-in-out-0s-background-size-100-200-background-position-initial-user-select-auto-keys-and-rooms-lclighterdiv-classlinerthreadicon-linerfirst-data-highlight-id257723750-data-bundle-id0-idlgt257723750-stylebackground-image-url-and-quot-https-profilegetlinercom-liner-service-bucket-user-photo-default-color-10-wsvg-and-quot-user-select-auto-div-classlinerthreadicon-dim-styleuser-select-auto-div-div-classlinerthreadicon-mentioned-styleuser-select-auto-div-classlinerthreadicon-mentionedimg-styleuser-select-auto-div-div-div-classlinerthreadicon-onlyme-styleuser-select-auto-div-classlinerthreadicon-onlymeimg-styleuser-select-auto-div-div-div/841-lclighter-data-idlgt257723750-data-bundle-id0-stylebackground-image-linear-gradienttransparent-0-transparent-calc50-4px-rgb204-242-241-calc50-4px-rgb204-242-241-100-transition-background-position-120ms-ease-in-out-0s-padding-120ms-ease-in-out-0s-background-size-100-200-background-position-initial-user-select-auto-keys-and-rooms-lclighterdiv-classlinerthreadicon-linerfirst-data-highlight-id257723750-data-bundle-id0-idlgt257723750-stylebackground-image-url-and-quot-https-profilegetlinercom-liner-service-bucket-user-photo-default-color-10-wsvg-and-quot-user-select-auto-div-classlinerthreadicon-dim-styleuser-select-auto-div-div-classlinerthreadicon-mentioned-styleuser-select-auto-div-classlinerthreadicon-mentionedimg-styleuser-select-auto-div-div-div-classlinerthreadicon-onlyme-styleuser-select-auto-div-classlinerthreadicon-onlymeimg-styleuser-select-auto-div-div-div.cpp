c9bb9832f76ac04b2595dea45ca4574db861c3c7
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-06-05 00:32:30 +0530

    Time: 20 ms (18.95%), Space: 10.4 MB (55.37%) - LeetHub

---
+++ 841-lclighter-data-idlgt257723750-data-bundle-id0-stylebackground-image-linear-gradienttransparent-0-transparent-calc50-4px-rgb204-242-241-calc50-4px-rgb204-242-241-100-transition-background-position-120ms-ease-in-out-0s-padding-120ms-ease-in-out-0s-background-size-100-200-background-position-initial-user-select-auto-keys-and-rooms-lclighterdiv-classlinerthreadicon-linerfirst-data-highlight-id257723750-data-bundle-id0-idlgt257723750-stylebackground-image-url-and-quot-https-profilegetlinercom-liner-service-bucket-user-photo-default-color-10-wsvg-and-quot-user-select-auto-div-classlinerthreadicon-dim-styleuser-select-auto-div-div-classlinerthreadicon-mentioned-styleuser-select-auto-div-classlinerthreadicon-mentionedimg-styleuser-select-auto-div-div-div-classlinerthreadicon-onlyme-styleuser-select-auto-div-classlinerthreadicon-onlymeimg-styleuser-select-auto-div-div-div/841-lclighter-data-idlgt257723750-data-bundle-id0-stylebackground-image-linear-gradienttransparent-0-transparent-calc50-4px-rgb204-242-241-calc50-4px-rgb204-242-241-100-transition-background-position-120ms-ease-in-out-0s-padding-120ms-ease-in-out-0s-background-size-100-200-background-position-initial-user-select-auto-keys-and-rooms-lclighterdiv-classlinerthreadicon-linerfirst-data-highlight-id257723750-data-bundle-id0-idlgt257723750-stylebackground-image-url-and-quot-https-profilegetlinercom-liner-service-bucket-user-photo-default-color-10-wsvg-and-quot-user-select-auto-div-classlinerthreadicon-dim-styleuser-select-auto-div-div-classlinerthreadicon-mentioned-styleuser-select-auto-div-classlinerthreadicon-mentionedimg-styleuser-select-auto-div-div-div-classlinerthreadicon-onlyme-styleuser-select-auto-div-classlinerthreadicon-onlymeimg-styleuser-select-auto-div-div-div.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void dfs(int i,vector<int>&v,vector<vector<int>>&r,int &c)
+    {
+        if(v[i]==1)
+            return;
+        v[i]=1;
+        c++;
+        for(auto x:r[i])
+            dfs(x,v,r,c);
+    }
+    bool canVisitAllRooms(vector<vector<int>>& r) {
+        int n=r.size();
+        vector<int>v(n,0);
+        int c=0;
+        dfs(0,v,r,c);
+        if(n==c)
+            return true;
+        return false;
+    }
+};
