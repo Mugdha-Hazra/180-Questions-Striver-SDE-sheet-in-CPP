55bdf4d5b22f88400c03f570ce526b05ca1a6d2f
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-06-04 21:10:10 +0530

    Time: 38 ms (75.61%), Space: 27.4 MB (5.90%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,10 +1,11 @@
+//Runtime: 35 ms, faster than 83.38% of C++ online submissions for Squares of a Sorted Array.
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& n) 
     {   int f=-1;
         for(int i=0;i<n.size();i++)
         {
-            n[i]=pow(n[i],2);
+            n[i]=n[i]*n[i];
         }
         stack<int>s;
         for(int i=0;i<n.size()-1;i++)
@@ -39,8 +40,7 @@ public:
             }
              return v;
         }
-       reverse(n.begin(),n.end());
-        //sort(n.begin(),n.end());
+        reverse(n.begin(),n.end());
         return n;
     }
 };
