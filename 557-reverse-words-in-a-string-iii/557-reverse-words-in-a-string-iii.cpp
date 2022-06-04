061ef23d4f60a1fd7b4103c82cf8484c8930ff89
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-06-04 20:14:51 +0530

    Time: 32 ms (16.91%), Space: 10.4 MB (36.83%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int j=0,k=0;
+        s+=' ';
+        for(int i=0;i<s.size();i++)
+        { 
+            if(s[i]!=' ')
+              j++;
+            else
+            {   cout<<" j"<<j<<" ";
+                cout<<"k"<<k<<" ";
+                reverse(s.begin()+k,s.begin()+j+k);
+                j=0;
+                k=i+1;
+            }
+            
+        }
+        s.pop_back();
+        return s;
+    }
+};
