98d5fd3d1ad675313cbe9bc6eab628ef8be402f0
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-06-05 17:24:39 +0530

    Time: 0 ms (100.00%), Space: 10.6 MB (75.79%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -17,8 +17,7 @@ public:
         for(int i=0;i<n;i++)
         p=p->next;
         while(p->next!=NULL)
-        {
-            p=p->next;
+        {   p=p->next;
             q=q->next;
         }
         q->next=q->next->next;
