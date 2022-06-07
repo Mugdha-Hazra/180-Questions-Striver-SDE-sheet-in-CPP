0175eb5c5160fd3588d6b96e9e2ce457254a6d4f
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-06-07 12:04:38 +0530

    Time: 182 ms (12.62%), Space: 54.8 MB (6.44%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& n) {
-        set<int>s;
-        for(auto x:n)
-            s.insert(x);
+        set<int>s(n.begin(),n.end());
+        //for(auto x:n)
+          //  s.insert(x);
         return s.size()!=n.size();
     }
 };
