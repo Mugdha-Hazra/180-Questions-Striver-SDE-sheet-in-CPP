3d737e81b88d47006fa69c394db708220e0168f1
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-06-13 10:51:56 +0530

    Time: 11 ms (38.22%), Space: 6 MB (41.52%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-    int fib(int n) 
+    int fib(int n)
     {
-        int a[n+1][n+1];
-        if(n<=1)
+        if(n==0||n==1)
             return n;
-        return a[n][n]=fib(n-1)+fib(n-2);
+        if(n==2||n==3)
+            return n-1;
+        return fib(n-2)+fib(n-1);
         
     }
 };
