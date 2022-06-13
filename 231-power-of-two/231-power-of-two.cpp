d4ca05ba6e05308a1a6442a7c005105dfc24ee27
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-06-13 08:02:49 +0530

    Time: 3 ms (56.65%), Space: 5.9 MB (25.12%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
-   // bool dp[10000000]={false};
     bool isPowerOfTwo(int n) {
         if(n==1)
             return true;
         if(n%2==1||n==0)
             return false;
-       // if(dp[n]==false)
-        return isPowerOfTwo(n/2);
-        
-//return dp[n];
+        return isPowerOfTwo(n/2);   
     }
 };
