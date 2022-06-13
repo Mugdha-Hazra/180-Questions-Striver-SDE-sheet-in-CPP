ec1ff899cf8f32ffd3c1dd31765ccf199460cec2
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-06-13 10:58:30 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (41.52%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
+    int dp[31]={0};
     int fib(int n)
-    {
-        if(n==0||n==1)
-            return n;
-        if(n==2||n==3)
-            return n-1;
-        return fib(n-2)+fib(n-1);
+    { 
+     
+        if(n==0||n==1||n==5)
+            return dp[n]=n;
+        if(n==2||n==3||n==4)
+            return dp[n]=n-1;
         
+         if(dp[n])
+             return dp[n];
+        
+         return dp[n]=fib(n-2)+fib(n-1);
+       
     }
 };
