251c441a3a0866475a66579efd2b2ec8b69c20c7
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-06-25 01:27:47 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (58.13%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int titleToNumber(string s) {
-        int n=0,x=0;
-        for(int i=0;i<s.size();i++)
-        {
-            n=n*26+(s[i]-'A')+1;
-        }
-            // {   n+=(s[i]-'A')+x+1;
-        //     cout<<n<<"*";
-        //     x=26*(s[i]-'A');
-        //}
+        int n=0;
+        for(int i=0;i<s.size();i++) n=n*26+(s[i]-'A')+1;
         return n;
     }
 };
