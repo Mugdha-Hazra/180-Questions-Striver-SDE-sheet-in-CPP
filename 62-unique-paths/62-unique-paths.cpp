6b819331aa8de44bf99dd9407bedc9fe475f7edb
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-06-26 22:38:17 +0530

    Time: 0 ms (100.00%), Space: 6 MB (73.04%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+       int dp[m+1][n+1];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+              if(i==0||j==0)
+                  dp[i][j]=1;
+            }
+        }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
