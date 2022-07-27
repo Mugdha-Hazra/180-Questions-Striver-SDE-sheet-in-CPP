ebd7f18d833358aa85ffaf86031899f500fea981
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-07-27 09:45:31 +0530

    Time: 13 ms (33.77%), Space: 12.8 MB (38.74%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -1,69 +1,29 @@
-/*
-// TC - O(N) 
-// SC - O(N)
-class Solution {
-    TreeNode* prev = NULL;
-public:
-    void flatten(TreeNode* root) {
-        if(root == NULL) return; 
-        
-        flatten(root->right); 
-        flatten(root->left); 
-        
-        root->right = prev;
-        root->left = NULL; 
-        prev = root; 
-    }
-};
-
-// TC - O(N) 
-// SC - O(N) 
-// Iterative 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void flatten(TreeNode* root) {
-        if(root == NULL) return; 
-        stack<TreeNode*> st; 
-        st.push(root); 
-        while(!st.empty()) {
-            TreeNode* cur = st.top(); 
-            st.pop(); 
-            
-            if(cur->right != NULL) {
-                st.push(cur->right); 
-            }
-            if(cur->left != NULL) {
-                st.push(cur->left); 
-            }
-            if(!st.empty()) {
-                cur->right = st.top(); 
-            }
-            cur->left = NULL;
-        }
-        
-    }
-};*/
-
-// TC - O(N) 
-// SC - O(1) 
-class Solution {
-public:
-    void flatten(TreeNode* root) {
-        TreeNode* cur = root;
-		while (cur)
-		{
-			if(cur->left)
-			{
-				TreeNode* pre = cur->left;
-				while(pre->right)
-				{
-					pre = pre->right;
-				}
-				pre->right = cur->right;
-				cur->right = cur->left;
-				cur->left = NULL;
-			}
-			cur = cur->right;
+   void flatten(TreeNode *root) {
+	while (root) {
+		if (root->left && root->right) {
+			TreeNode* t = root->left;
+			while (t->right)
+				t = t->right;
+			t->right = root->right;
 		}
-    }
+
+        if(root->left)
+		    root->right = root->left;
+		root->left = NULL;
+		root = root->right;
+	}
+}
 };
