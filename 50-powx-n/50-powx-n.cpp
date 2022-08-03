f96bb2757e0ef4c4da766374e60dbb74ff4c7d11
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-08-04 01:38:44 +0530

    Time: 3 ms (47.73%), Space: 6 MB (43.77%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -3,7 +3,8 @@ public:
     double myPow(double x, int n) {
         double res = 1;
         while (n) {
-            if (n % 2) res = n > 0 ? res * x : res / x;
+            if (n % 2)
+                res = n > 0 ? res * x : res / x;
             x = x * x;
             n /= 2;
         }
