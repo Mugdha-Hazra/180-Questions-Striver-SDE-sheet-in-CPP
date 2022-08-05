f00616b048150d69b50eb74ad5822871cd4071d9
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-08-05 23:29:04 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (65.95%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+     int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> result(target + 1);
+        result[0] = 1;
+        for (int i = 1; i <= target; ++i) {
+            for (int x : nums) {
+                if (i >= x) result[i] += result[i - x];
+            }
+        }
+        
+        return result[target];
+    }
+};
