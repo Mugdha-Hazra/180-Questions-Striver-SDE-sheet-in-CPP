41eadf70b2825114a3263bb723a47e58bdf23984
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-08-27 01:28:22 +0530

    Time: 3 ms (33.14%), Space: 6 MB (45.39%) - LeetHub

---
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string convertToTitle(int c) {
+        string s;
+        if(c==1)
+            return "A";
+        while(c>0)
+        {
+          int  r=c%26;
+            if(r==0)
+            {s+='Z';
+            c=c-26;
+            }
+           else
+                     s+='A'+r-1;
+             c=c/26;
+            
+        }
+        // if(c==1)
+        //     s+="A";
+        reverse(s.begin(),s.end());
+        return s;
+    }
+    
+};
+
