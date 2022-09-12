2fe6b79cabde91ec4a44702e094b86980e2eac54
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-09-12 23:36:34 +0530

    Time: 3 ms (98.28%), Space: 10.7 MB (27.39%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+     int bagOfTokensScore(vector<int>& tokens, int P) {
+        sort(tokens.begin(), tokens.end());
+        int res = 0, points = 0, i = 0, j = tokens.size() - 1;
+        while (i <= j) {
+            if (P >= tokens[i]) {
+                P -= tokens[i++];
+                res = max(res, ++points);
+            } else if (points > 0) {
+                points--;
+                P += tokens[j--];
+            } else {
+                break;
+            }
+        }
+        return res;
+    }
+};
