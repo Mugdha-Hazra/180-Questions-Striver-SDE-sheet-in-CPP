0732df4f6e66b2b9cc831171b20547f408f78606
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-09-13 09:19:19 +0530

    Time: 26 ms (31.17%), Space: 14 MB (38.65%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int count = 0;
+        for (auto c : data) {
+            if (count == 0) {
+                if ((c >> 5) == 0b110) count = 1;
+                else if ((c >> 4) == 0b1110) count = 2;
+                else if ((c >> 3) == 0b11110) count = 3;
+                else if ((c >> 7)) return false;
+            } else {
+                if ((c >> 6) != 0b10) return false;
+                count--;
+            }
+        }
+        return count == 0;
+    }
+};
