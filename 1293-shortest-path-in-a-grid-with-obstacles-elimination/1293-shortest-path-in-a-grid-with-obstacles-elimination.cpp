e6998c4d2ebeb7a51386ca2e189532b7a00e480d
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-10-30 22:45:42 +0530

    Time: 387 ms (39.41%), Space: 50.7 MB (35.76%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int solve(vector<vector<int>>& grid,int k){
+		// At a particular cell we will store the number of obstacles that we can still remove after walking through that cell
+        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),-1));
+        queue<vector<int>> q;
+		
+		// queue stores (x,y,current path length,number of obstacles we can still remove)
+        q.push({0,0,0,k});
+        while(!q.empty()){
+            auto t=q.front();
+            int x=t[0],y=t[1];
+            q.pop();
+			
+			// Exit if current position is outside of the grid
+            if(x<0 || y<0 || x>=grid.size() || y>=grid[0].size()){
+                continue;
+            }
+			
+			// Destination found
+            if(x==grid.size()-1 && y==grid[0].size()-1)
+                return t[2];
+
+            if(grid[x][y]==1){
+                if(t[3]>0) // If we encounter an obstacle and we can remove it
+                    t[3]--;
+                else
+                    continue;
+            }
+			
+			// The cell was previously visited by some path and we were able to remove more obstacles from the previous path.
+			// Then we don't need to continue on our current path
+            if(vis[x][y]!=-1 && vis[x][y]>=t[3])
+                continue;
+            vis[x][y]=t[3];
+            
+            q.push({x+1,y,t[2]+1,t[3]});
+            q.push({x,y+1,t[2]+1,t[3]});
+            q.push({x-1,y,t[2]+1,t[3]});
+            q.push({x,y-1,t[2]+1,t[3]});
+            
+        }
+        return -1;
+    }
+    
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        return solve(grid,k);
+    }
+};
