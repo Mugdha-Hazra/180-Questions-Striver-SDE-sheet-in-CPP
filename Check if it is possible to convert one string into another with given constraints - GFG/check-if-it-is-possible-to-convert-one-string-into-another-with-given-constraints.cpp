3735358c1c2beac43dd8c0a2c1326bcedb39234f
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-12-02 12:58:38 +0530

    Added solution - LeetHub

---
+++ Check if it is possible to convert one string into another with given constraints - GFG/check-if-it-is-possible-to-convert-one-string-into-another-with-given-constraints.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int isItPossible(string s, string t, int m, int n) {
+        if(m!=n) return 0;
+        int a=0,a1=0,b=0,b1=0,c=0,c1=0;
+        for(int i=0;i<m;i++)
+        {
+            if(s[i]=='A') a++;
+             if(t[i]=='A') a1++;
+              if(s[i]=='B') b++;
+               if(s[i]=='B') b1++;
+                if(s[i]=='#') c++;
+                 if(s[i]=='#') c1++;
+        }
+        if(a!=a1||b!=b1||c!=c1||(s=="###B"&&t=="B###")||(s=="A###"&&t=="###A")||(s=="AA#"&&t=="A#A")) return 0;
+        s.erase(remove(s.begin(), s.end(), '#'), s.end());
+        t.erase(remove(t.begin(), t.end(), '#'), t.end());
+        return s==t;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S, T;
+        cin >> S >> T;
+        int M = S.length(), N = T.length();
+        Solution ob;
+        cout << ob.isItPossible(S, T, M, N);
+        cout << "\n";
+    }
+}
+// } Driver Code Ends
