d393cd8965b7b6b35bcbb32c537f9a39c837613d
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-12-02 12:59:58 +0530

    Added solution - LeetHub

--- Check if it is possible to convert one string into another with given constraints - GFG/check-if-it-is-possible-to-convert-one-string-into-another-with-given-constraints.cpp
+++ Check if it is possible to convert one string into another with given constraints - GFG/check-if-it-is-possible-to-convert-one-string-into-another-with-given-constraints.cpp
@@ -10,17 +10,6 @@ using namespace std;
 class Solution {
   public:
     int isItPossible(string s, string t, int m, int n) {
-        // if(m!=n) return 0;
-        // int a=0,a1=0,b=0,b1=0,c=0,c1=0;
-        // for(int i=0;i<m;i++)
-        // {
-        //     if(s[i]=='A') a++;
-        //      if(t[i]=='A') a1++;
-        //       if(s[i]=='B') b++;
-        //       if(s[i]=='B') b1++;
-        //         if(s[i]=='#') c++;
-        //          if(s[i]=='#') c1++;
-        // }
         if((s=="###B"&&t=="B###")||(s=="A###"&&t=="###A")||(s=="AA#"&&t=="A#A")) return 0;
         s.erase(remove(s.begin(), s.end(), '#'), s.end());
         t.erase(remove(t.begin(), t.end(), '#'), t.end());
