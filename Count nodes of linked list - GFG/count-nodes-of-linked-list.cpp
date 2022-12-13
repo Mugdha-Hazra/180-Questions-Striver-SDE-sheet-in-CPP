96f595f77520872ec07e21e84e083f3dba96449b
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-12-13 06:10:45 +0530

    Added solution - LeetHub

---
+++ Count nodes of linked list - GFG/count-nodes-of-linked-list.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/* Link list node */
+/*
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int x) {  data = x;  next = NULL; }
+}; */
+
+class Solution
+{
+    public:
+    //Function to count nodes of a linked list.
+    int getCount(struct Node* head){
+    if(head==NULL)return 0;
+    int c=0;
+    Node*p=head;
+    while(p!=NULL)
+    {c++;
+        p=p->next;
+    }
+    return c;
+    }
+};
+    
+
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < n-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        Solution ob;
+        cout << ob.getCount(head) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
