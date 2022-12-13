f802269cbf4b44c3b805086f9928b5e3600973a7
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-12-13 07:01:29 +0530

    Added solution - LeetHub

--- Implement strstr - GFG/implement-strstr.cpp
+++ Implement strstr - GFG/implement-strstr.cpp
@@ -26,6 +26,24 @@ int main()
 //Function to locate the occurrence of the string x in the string s.
 int strstr(string s, string x)
 {
-    return s.find(x);
+    // return s.find(x);
+    // return -1;
+    int count=0,index=0,i,j,ocur=0;
+    if(x.size()>s.size())
+        return -1;
+    if(x.size()<=0)
+        return -1;
+    for(i=0;i<s.size();i++)
+    {
+        index=i;
+        if(s[i]==x[0])
+        {
+        for(j=0;j<x.size();j++)
+                if(s[i+j]==x[j])
+                    count++;
+        if(count==j)
+            return index;
+        count=0;}
+    }
     return -1;
 }
