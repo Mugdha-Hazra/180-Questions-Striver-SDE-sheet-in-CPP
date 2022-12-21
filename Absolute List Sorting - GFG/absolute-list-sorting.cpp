c72194d1ca75f77ff2be493a54f995d67110bb08
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-12-21 15:13:36 +0530

    Added solution - LeetHub

---
+++ Absolute List Sorting - GFG/absolute-list-sorting.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Linked List Node
+struct Node
+{
+	Node* next;
+	int data;
+};
+
+// Utility function to insert a node at the
+// beginning
+void push(Node** head, int data)
+{
+	Node* newNode = new Node;
+	newNode->next = (*head);
+	newNode->data = data;
+	(*head) = newNode;
+}
+
+// Utility function to print a linked list
+void printList(Node* head)
+{
+	while (head != NULL)
+	{
+		cout << head->data;
+		if (head->next != NULL)
+			cout << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
+
+// } Driver Code Ends
+/* The structure of the Linked list Node is as follows:
+struct Node
+{
+    Node* next;
+    int data;
+}; */
+
+/*Your method shouldn't print anything
+ it should transform the passed linked list */
+class Solution{
+    
+public:
+   Node* sortList(Node* head)
+    {
+        Node* curr = head;
+        while(curr->next != NULL){
+            if(curr->next->data < 0){
+                Node* next = curr->next->next;
+                curr->next->next = head;
+                head = curr->next;
+                curr->next = next;
+                
+            }else{
+                curr = curr->next;
+            }
+        }
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+
+	int t = 0;
+	int n = 0;
+	cin >> t;
+	while (t--)
+	{
+		Node* head = NULL;
+		cin >> n;
+		int arr[n];
+		for (int i = 0; i < n; i++)
+		{
+			cin >> arr[i];
+			// push(&head, a);
+		}
+		for (int i = n - 1; i >= 0; i--)
+		{
+			push(&head, arr[i]);
+		}
+		// printList(head);
+		
+		Solution ob;
+		head=ob.sortList(head);
+
+		printList(head);
+
+	}
+	return 0;
+}
+
+// } Driver Code Ends
