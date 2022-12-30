fe30827d97fb399df30d99ab4d5db908c2cf9d04
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-12-30 09:54:02 +0530

    Added solution - LeetHub

---
+++ Single valued subtree - GFG/single-valued-subtree.cpp
@@ -0,0 +1,151 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+	int data;
+	Node* left;
+	Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+	Node* temp = new Node;
+	temp->data = val;
+	temp->left = NULL;
+	temp->right = NULL;
+
+	return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node* root = newNode(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current node
+			currNode->left = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/* Tree Node
+struct Node
+{
+	int data;
+	Node* left;
+	Node* right;
+};*/
+
+class Solution
+{
+    public:
+    int ans = 0;
+    bool solve(Node* root){
+        if(root==NULL)return 1;
+        bool ok=0;
+        bool ch1 = solve(root->left);
+        bool ch2 = solve(root->right);
+        ok = ch1 && ch2;
+        if(root->left && root->data != root->left->data)ok=0;
+        if(root->right && root->data != root->right->data)ok=0;
+        if(ok)ans++;
+        return ok;
+    }
+    
+    int singlevalued(Node *root)
+    {
+        //code here
+        solve(root);
+        return ans;
+    }
+    
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+	int t;
+	cin >> t;
+	getchar();
+	while (t--)
+	{
+
+		string inp;
+		getline(cin, inp);
+
+        Solution ob;
+        
+		struct Node* root = buildTree(inp);
+		cout << ob.singlevalued(root) << "\n";
+
+	}
+	return 0;
+}
+
+// } Driver Code Ends
