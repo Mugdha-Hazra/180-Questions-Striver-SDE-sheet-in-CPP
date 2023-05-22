4bc7bc85d66971631c81dbf3fd0ae0936b72783b
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2023-05-22 20:41:08 +0530

    Added solution - LeetHub

---
+++ Tree Transformation - GFG/tree-transformation.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+    public:
+    int solve(int n, vector<int> p){
+        // code here
+        int i,ans=n-1;
+        vector<int>degree(n+1,0);
+        for(i=1;i<n;i++){
+            degree[p[i]]++;
+            degree[i]++;
+        }
+        for(i=0;i<n;i++)if(degree[i]==1)ans--;
+        
+        if(ans<0)return 0;
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int N;
+        cin >> N;
+        vector<int> p(N);
+        for(int i = 0; i < N; i++){
+            cin >> p[i];
+        }
+
+        Solution obj;
+        cout << obj.solve(N, p) << "\n";
+    }
+}   
+// } Driver Code Ends
