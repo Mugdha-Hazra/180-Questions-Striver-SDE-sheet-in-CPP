bfe4c0d674caedf6087c5e96ffdbf529c40d8309
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2023-06-05 23:25:54 +0530

    Added solution - LeetHub

---
+++ Find the Closest Element in BST - GFG/find-the-closest-element-in-bst.cpp
@@ -0,0 +1,152 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+
+// } Driver Code Ends
+/*
+Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution
+{
+    public:
+    
+    void find(Node *root,vector<int>&v,int k){
+        if(root==NULL)return;
+        find(root->left,v,k);
+        v.push_back(abs(root->data-k));
+        find(root->right,v,k);
+    }
+    
+    //Function to find the least absolute difference between any node
+    //value of the BST and the given integer.
+    int minDiff(Node *root, int K)
+    {
+        vector<int>v;
+        find(root,v,K);
+        sort(v.begin(),v.end());
+        return v[0];
+        //Your code here
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+
+
+int main() {
+   
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+        string s; 
+       getline(cin, s);
+       Node* root1 = buildTree(s);
+
+       getline(cin, s);
+       int k = stoi(s);
+       // getline(cin, s);
+
+        Solution ob;
+       cout << ob.minDiff(root1, k);
+        cout << endl;
+       //cout<<"~"<<endl;
+   }
+   return 0;
+}
+
+// } Driver Code Ends
