c80060c8d7eb69a64022a59be23798582df92a18
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2023-08-30 23:37:21 +0530

    Added solution - LeetHub

---
+++ Delete a Node in Single Linked List - GFG/delete-a-node-in-single-linked-list.cpp
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** head_ref, struct Node **tail_ref,
+           int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+/* Function to get the middle of the linked list*/
+struct Node* deleteNode(struct Node *head,int );
+
+void printList(Node *head)
+{
+   while (head != NULL)
+   {
+      cout << head->data << " ";
+      head = head->next;
+   }
+   cout << "\n";
+}
+
+/* Driver program to test above function*/
+int main()
+{
+  int T, i, n, l;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+
+	int kk;
+	cin>>kk;
+    head = deleteNode(head,kk);
+    printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Link list Node 
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+/*You are required to complete below method*/
+Node* deleteNode(Node *head,int x)
+{
+    Node*temp=head;
+    if(x==1)  return temp->next;
+    while(x-->2)
+    {    
+        temp=temp->next;
+    }
+    temp->next=temp->next->next;
+    return head;
+}
